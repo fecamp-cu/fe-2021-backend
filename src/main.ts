394f72e97b0130fcafe81a3cebe4216a70286ef5
samithiwat <samithiwat2544@gmail.com>
2021-12-11 19:10:59 +0700

    Edit Main Config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get<number>('port'));
 }
 bootstrap();
