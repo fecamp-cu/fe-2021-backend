e92eba379b106e50b68a8528bc18b6aab0d31c29
samithiwat <samithiwat2544@gmail.com>
2022-01-05 17:59:22 +0700

    Setup OpenAPI Doc

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('FE Camp API')
+    .setDescription('FE API Docs since 2022')
+    .setVersion('1.0')
+    .addTag('Auth')
+    .addTag('User')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(configService.get<number>('port'));
 }
