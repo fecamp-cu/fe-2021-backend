619679bc27a7a915e331cd51b0cb4ecd21cd66a0
sern <sern2001@hotmail.com>
2022-01-06 16:51:19 +0700

    Mapped table profile

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Profile } from 'src/profile/entities/profile.entity';
 import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Profile])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
