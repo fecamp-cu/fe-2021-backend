3901d46c03ab11e42150a1c54b6d6290d284582a
samithiwat <samithiwat2544@gmail.com>
2022-01-08 05:05:55 +0700

    Refactor auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -23,14 +23,15 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  private client: GoogleAuthentication;
+  private googleClient: GoogleAuthentication;
   constructor(
     private readonly authService: AuthService,
     private userService: UserService,
     private configService: ConfigService,
   ) {
-    this.client = new GoogleAuthentication(this.configService);
+    this.googleClient = new GoogleAuthentication(this.configService);
   }
+
   @Post('register')
   async register(@Body() registerDto: RegisterDto, @Res() res: Response): Promise<Response> {
     const user: UserDto = await this.userService.create(registerDto);
@@ -38,6 +39,7 @@ export class AuthController {
       .status(HttpStatus.CREATED)
       .json({ message: 'Successfully registered user', ...user });
   }
+
   @Post('login')
   async login(@Body() loginDto: LoginDto, @Res() res: Response): Promise<Response> {
     const user = await this.userService.Login(loginDto);
@@ -45,12 +47,14 @@ export class AuthController {
     res.cookie('access_token', token, { httpOnly: true, secure: false });
     return res.status(HttpStatus.OK).json(user);
   }
+
   @Get('me')
   @UseGuards(JwtAuthGuard)
   async profile(@Req() req, @Res() res: Response): Promise<Response> {
     const user = await this.userService.findOne(req.user.id);
     return res.status(HttpStatus.OK).json(user);
   }
+
   @Delete('logout')
   @HttpCode(HttpStatus.NO_CONTENT)
   @UseGuards(JwtAuthGuard)
