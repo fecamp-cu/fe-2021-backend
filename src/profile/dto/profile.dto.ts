92eb3a58fa161b2deac28a6e0d2fd849795da12c
samithiwat <samithiwat2544@gmail.com>
2022-01-08 05:04:31 +0700

    Add profile DTO

---
+++ src/profile/dto/profile.dto.ts
@@ -0,0 +1,47 @@
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsPostalCode } from 'class-validator';
+
+export class ProfileDto {
+  id: number;
+
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  imageUrl: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber('TH')
+  tel: string;
+
+  @IsNotEmpty()
+  grade: string;
+
+  @IsNotEmpty()
+  school: string;
+
+  @IsNotEmpty()
+  address: string;
+
+  @IsNotEmpty()
+  subdistrict: string;
+
+  @IsNotEmpty()
+  district: string;
+
+  @IsNotEmpty()
+  province: string;
+
+  @IsNotEmpty()
+  @IsPostalCode()
+  postcode: string;
+
+  constructor(partial: Partial<ProfileDto>) {
+    Object.assign(this, partial);
+  }
+}
