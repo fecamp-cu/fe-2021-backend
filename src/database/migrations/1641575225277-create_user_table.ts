051a725396b49f76925e71eb7ceaf5acfd8028b7
sern <sern2001@hotmail.com>
2022-01-08 00:36:53 +0700

    Add user migration

--- src/database/migrations/1641575225277-create_user_table.ts
+++ src/database/migrations/1641575225277-create_user_table.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class createUserTable1641575225277 implements MigrationInterface {
-    name = 'createUserTable1641575225277'
+  name = 'createUserTable1641575225277';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "role" character varying NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" ADD "role" character varying NOT NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "role"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "role"`);
+  }
 }
