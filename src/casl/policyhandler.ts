fcce2ffd8d4b3100390171562069fc76c32bea64
sern <sern2001@hotmail.com>
2022-01-08 12:42:45 +0700

    Add policiesguard & policyhandler

---
+++ src/casl/policyhandler.ts
@@ -0,0 +1,41 @@
+import { SetMetadata } from '@nestjs/common';
+import { Action } from 'src/common/enums/action';
+import { AppAbility } from './casl-ability.factory';
+
+interface IPolicyHandler {
+  handle(ability: AppAbility): boolean;
+}
+
+type PolicyHandlerCallback = (ability: AppAbility) => boolean;
+
+export type PolicyHandler = IPolicyHandler | PolicyHandlerCallback;
+export const CHECK_POLICIES_KEY = 'check_policy';
+export const CheckPolicies = (...handlers: PolicyHandler[]) =>
+  SetMetadata(CHECK_POLICIES_KEY, handlers);
+
+export class ManagePolicyHandler implements IPolicyHandler {
+  handle(ability: AppAbility) {
+    return ability.can(Action.MANAGE, 'all');
+  }
+}
+export class ReadPolicyHandler implements IPolicyHandler {
+  handle(ability: AppAbility) {
+    return ability.can(Action.READ, 'all');
+  }
+}
+export class CreatePolicyHandler implements IPolicyHandler {
+  handle(ability: AppAbility) {
+    return ability.can(Action.CREATE, 'all');
+  }
+}
+export class UpdatePolicyHandler implements IPolicyHandler {
+  handle(ability: AppAbility) {
+    return ability.can(Action.UPDATE, 'all');
+  }
+}
+
+export class DeletePolicyHandler implements IPolicyHandler {
+  handle(ability: AppAbility) {
+    return ability.can(Action.DELETE, 'all');
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { editProfileDto } from 'src/auth/dto/edit-profile.dto';
 import { RegisterDto } from 'src/auth/dto/register.dto';
+import { CaslAbilityFactory } from 'src/casl/casl-ability.factory';
+import { PoliciesGuard } from 'src/casl/policyguard';
+import { CheckPolicies, ReadPolicyHandler } from 'src/casl/policyhandler';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UserService } from './user.service';
 
 @Controller('user')
 @ApiTags('User')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private caslAbilityFactory: CaslAbilityFactory,
+  ) {}
 
   @Post()
   create(@Body() registerDto: RegisterDto) {
@@ -15,9 +22,20 @@ export class UserController {
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new ReadPolicyHandler())
   findAll() {
     return this.userService.findAll();
   }
+  // async findAll(@Req() req) {
+  //   const user = await this.userService.findOne(req.user.id);
+  //   const ability = await this.caslAbilityFactory.createForUser(user);
+  //   if (ability.can(Action.CREATE, 'all')) {
+  //     return this.userService.findAll();
+  //   }
+  //   throw new HttpException('Insufficiency permission', HttpStatus.FORBIDDEN);
+  // }
 
   @Get(':id')
   findById(@Param('id') id: string) {
