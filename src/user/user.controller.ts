8767a4dc3147d6d2cfc994b0602533faeb49478d
sern <sern2001@hotmail.com>
2022-01-09 02:33:38 +0700

    Fix controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,9 +8,7 @@ import { CheckPolicies, ManagePolicyHandler } from 'src/casl/policyhandler';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UserService } from './user.service';
 
-@UseGuards(JwtAuthGuard,PoliciesGuard)
-
-@CheckPolicies(new ManagePolicyHandler())
+@UseGuards(JwtAuthGuard)
 @Controller('user')
 @ApiTags('User')
 export class UserController {
@@ -18,27 +16,32 @@ export class UserController {
     private readonly userService: UserService,
     private caslAbilityFactory: CaslAbilityFactory,
   ) {}
-
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new ManagePolicyHandler())
   @Post()
   create(@Body() registerDto: RegisterDto) {
     return this.userService.create(registerDto);
   }
-
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new ManagePolicyHandler())
   @Get()
   findAll() {
     return this.userService.findAll();
   }
-
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new ManagePolicyHandler())
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
-
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new ManagePolicyHandler())
   @Patch(':id')
   update(@Param('id') id: string, @Body() editProfileDto: editProfileDto) {
     return this.userService.update(+id, editProfileDto);
   }
-
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new ManagePolicyHandler())
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
