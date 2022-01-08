46e5c3a04ba31a3af2a8224e04ef5a354289a66f
sern <sern2001@hotmail.com>
2022-01-08 12:56:00 +0700

    Add managePolicyHandler in userController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,8 +3,8 @@ import { ApiTags } from '@nestjs/swagger';
 import { editProfileDto } from 'src/auth/dto/edit-profile.dto';
 import { RegisterDto } from 'src/auth/dto/register.dto';
 import { CaslAbilityFactory } from 'src/casl/casl-ability.factory';
-import { PoliciesGuard } from 'src/casl/policyguard';
-import { CheckPolicies, ReadPolicyHandler } from 'src/casl/policyhandler';
+import { PoliciesGuard } from 'src/casl/policies.guard';
+import { CheckPolicies, ManagePolicyHandler } from 'src/casl/policyhandler';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UserService } from './user.service';
 
@@ -22,20 +22,12 @@ export class UserController {
   }
 
   @Get()
-  @UseGuards(JwtAuthGuard)
   @UseGuards(PoliciesGuard)
-  @CheckPolicies(new ReadPolicyHandler())
+  @CheckPolicies(new ManagePolicyHandler())
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.userService.findAll();
   }
-  // async findAll(@Req() req) {
-  //   const user = await this.userService.findOne(req.user.id);
-  //   const ability = await this.caslAbilityFactory.createForUser(user);
-  //   if (ability.can(Action.CREATE, 'all')) {
-  //     return this.userService.findAll();
-  //   }
-  //   throw new HttpException('Insufficiency permission', HttpStatus.FORBIDDEN);
-  // }
 
   @Get(':id')
   findById(@Param('id') id: string) {
