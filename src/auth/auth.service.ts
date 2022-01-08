90d32cdfdf0e488d30fd526e7c78a2181b6d784c
samithiwat <samithiwat2544@gmail.com>
2022-01-08 08:08:00 +0700

    Add createUser function in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { ProfileService } from 'src/profile/profile.service';
 import { UserDto } from 'src/user/dto/user.dto';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService, private jwtService: JwtService) {}
+  constructor(
+    private userService: UserService,
+    private profileService: ProfileService,
+  ) {}
 
   async createToken(user: UserDto) {
     const payload = {
       id: user.id,
     };
     return this.jwtService.sign(payload);
+  }
+
+  public async createUser(registerDto: RegisterDto): Promise<UserDto> {
+    const profile: Profile = await this.profileService.create(registerDto.userInfo);
+    const user = await this.userService.create(registerDto.credentials, profile);
+
+    return user;
   }
 }
