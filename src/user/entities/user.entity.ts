cdaeb9f31c98044450243a4306303ca8e2858c86
sern <sern2001@hotmail.com>
2022-01-08 21:40:07 +0700

    Fix migration file

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -25,7 +25,7 @@ export class User {
   @Column({ unique: true })
   email: string;
 
-  @Column()
+  @Column({ default: 'user' })
   role: Role;
 
   @CreateDateColumn({ name: 'created_date', select: false })
