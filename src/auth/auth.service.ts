54a115d2618ce73d833ca6df69ca83b69fc88062
samithiwat <samithiwat2544@gmail.com>
2022-01-08 08:08:18 +0700

    Add storeToken in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,4 +24,46 @@ export class AuthService {
 
     return user;
   }
+
+  public async storeToken(
+    tokens: OAuthResponse,
+    serviceType: ServiceType,
+    user: UserDto,
+  ): Promise<UserDto> {
+    const tokenDto = new TokenDto({
+      accessToken: await crypto.AES.encrypt(
+        tokens.access_token,
+        this.configService.get<string>('encryptionKey'),
+      ).toString(),
+
+      refreshToken: await crypto.AES.encrypt(
+        tokens.refresh_token,
+        this.configService.get<string>('encryptionKey'),
+      ).toString(),
+
+      expiresDate: new Date(tokens.expiry_date),
+      serviceType,
+      user,
+    });
+
+    let token: TokenDto;
+    if (user.tokens) {
+      token = user.tokens.find(token => token.serviceType === serviceType);
+    }
+
+    if (!token) {
+      token = await this.tokenService.create(tokenDto);
+      if (!user.tokens) {
+        user.tokens = [];
+      }
+      user.tokens = user.tokens.concat(token);
+
+      return await this.userRepository.save(user);
+    }
+
+    token = await this.tokenService.update(token.id, tokenDto);
+    user.tokens.filter(token => token.serviceType !== serviceType).concat(token);
+
+    return await this.userRepository.save(user);
+  }
 }
