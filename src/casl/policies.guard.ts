e9b938b6eb960a9cbe95874bb411a4a134a80a59
sern <sern2001@hotmail.com>
2022-01-09 14:43:53 +0700

    Add configService for getting policykey from  env

--- src/casl/policies.guard.ts
+++ src/casl/policies.guard.ts
@@ -1,8 +1,9 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Reflector } from '@nestjs/core';
 import { UserService } from 'src/user/user.service';
 import { AppAbility, CaslAbilityFactory } from './casl-ability.factory';
-import { CHECK_POLICIES_KEY, PolicyHandler } from './policyhandler';
+import { PolicyHandler } from './policyhandler';
 
 @Injectable()
 export class PoliciesGuard implements CanActivate {
@@ -10,11 +11,15 @@ export class PoliciesGuard implements CanActivate {
     private reflector: Reflector,
     private caslAbilityFactory: CaslAbilityFactory,
     private userService: UserService,
+    private configService: ConfigService,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const policyHandlers =
-      this.reflector.get<PolicyHandler[]>(CHECK_POLICIES_KEY, context.getHandler()) || [];
+      this.reflector.get<PolicyHandler[]>(
+        this.configService.get<string>('poilcykey'),
+        context.getHandler(),
+      ) || [];
     const req = context.switchToHttp().getRequest();
     const user = await this.userService.findOne(req.user.id);
     const ability = this.caslAbilityFactory.createForUser(user);
