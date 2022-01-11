1b5259b3053a05b59b05c6995ca4b68da366b526
samithiwat <samithiwat2544@gmail.com>
2022-01-12 03:58:08 +0700

    Add ParseInt Pipe to prevent 500 response because invalid input type

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { RegisterDto } from 'src/auth/dto/register.dto';
-import { CaslAbilityFactory } from 'src/casl/casl-ability.factory';
 import { PoliciesGuard } from 'src/casl/policies.guard';
 import { CheckPolicies, ManagePolicyHandler } from 'src/casl/policyhandler';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
@@ -12,38 +21,40 @@ import { UserService } from './user.service';
 @Controller('user')
 @ApiTags('User')
 export class UserController {
-  constructor(
-    private readonly userService: UserService,
-    private caslAbilityFactory: CaslAbilityFactory,
-  ) {}
+  constructor(private readonly userService: UserService) {}
+
   @UseGuards(PoliciesGuard)
   @CheckPolicies(new ManagePolicyHandler())
   @Post()
   create(@Body() registerDto: RegisterDto) {
     return this.userService.create(registerDto.credentials);
   }
+
   @UseGuards(PoliciesGuard)
   @CheckPolicies(new ManagePolicyHandler())
   @Get()
   findAll() {
     return this.userService.findAll();
   }
+
   @UseGuards(PoliciesGuard)
   @CheckPolicies(new ManagePolicyHandler())
   @Get(':id')
-  findById(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  findById(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
   }
+
   @UseGuards(PoliciesGuard)
   @CheckPolicies(new ManagePolicyHandler())
   @Patch(':id')
-  update(@Param('id') id: string, @Body() userDto: UserDto) {
-    return this.userService.update(+id, userDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() userDto: UserDto) {
+    return this.userService.update(id, userDto);
   }
+
   @UseGuards(PoliciesGuard)
   @CheckPolicies(new ManagePolicyHandler())
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.remove(id);
   }
 }
