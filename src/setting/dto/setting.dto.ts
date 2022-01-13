2d152255c9e9e74a7f211e8fd7a07d1432dd15d7
sern <sern2001@hotmail.com>
2022-01-14 01:48:44 +0700

    Add setting service

---
+++ src/setting/dto/setting.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean } from 'class-validator';
+
+export class SettingDto {
+  id: number;
+
+  @ApiProperty()
+  youtube_url: string;
+
+  @ApiProperty()
+  register_form_url: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  is_active: boolean;
+  constructor(partial: Partial<SettingDto>) {
+    Object.assign(this, partial);
+  }
+}
