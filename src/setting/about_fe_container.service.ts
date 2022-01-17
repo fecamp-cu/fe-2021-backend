3773060549010a7e28b911c7658e314bf534ab1f
sern <sern2001@hotmail.com>
2022-01-18 00:59:49 +0700

    finish all setting service

---
+++ src/setting/about_fe_container.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { AboutFeContainerDto } from './dto/about_fe_container.dto';
+import { AboutFeContainer } from './entities/about_fe_container.entity';
+import { Setting } from './entities/setting.entity';
+import { SettingService } from './setting.service';
+
+@Injectable()
+export class AboutFeContainerService {
+  constructor(
+    @InjectRepository(Setting) private settingRepository: Repository<Setting>,
+    @InjectRepository(AboutFeContainer)
+    private aboutFeContainerRepository: Repository<AboutFeContainer>,
+    private readonly settingService: SettingService,
+  ) {}
+  async create(
+    aboutFeContainerDto: AboutFeContainerDto,
+    settingid: number,
+  ): Promise<AboutFeContainerDto> {
+    const aboutFeContainer: AboutFeContainer = await this.aboutFeContainerRepository.create(
+      aboutFeContainerDto,
+    );
+    const setting: Setting = await this.settingService.findOne(settingid);
+    aboutFeContainer.setting = setting;
+    const createdAboutFeContainer = await this.aboutFeContainerRepository.save(aboutFeContainer);
+    return createdAboutFeContainer;
+  }
+
+  async findAll(): Promise<AboutFeContainerDto[]> {
+    return await this.aboutFeContainerRepository.find();
+  }
+
+  async findOne(id: number, relations: string[] = []): Promise<AboutFeContainer> {
+    const aboutFeContainer: AboutFeContainer = await this.aboutFeContainerRepository.findOne(id, {
+      relations,
+    });
+    if (!AboutFeContainer) {
+      throw new NotFoundException({
+        reason: 'NOT_FOUND_ENTITY',
+        message: 'Not found timeline_event',
+      });
+    }
+    return aboutFeContainer;
+  }
+
+  async update(
+    id: number,
+    aboutFeContainerDto: AboutFeContainerDto,
+    relations: string[] = [],
+  ): Promise<AboutFeContainerDto> {
+    const update: UpdateResult = await this.aboutFeContainerRepository.update(
+      id,
+      aboutFeContainerDto,
+    );
+    if (update.affected === 0) {
+      throw new NotFoundException({
+        reason: 'NOT_FOUND',
+        message: 'Not found timeline_event',
+      });
+    }
+    return await this.findOne(id, relations);
+  }
+
+  async remove(id: number): Promise<AboutFeContainerDto> {
+    const deleted: DeleteResult = await this.aboutFeContainerRepository.softDelete(id);
+    if (deleted.affected === 0) {
+      throw new NotFoundException({
+        reason: 'NOT_FOUND',
+        message: 'Not found timeline_event',
+      });
+    }
+    const aboutFeContainer = await this.findOne(id);
+    return aboutFeContainer;
+  }
+}
--- src/setting/setting.module.ts
+++ src/setting/setting.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AboutFeContainerService } from './about_fe_container.service';
 import { AboutFeContainer } from './entities/about_fe_container.entity';
 import { PhotoPreview } from './entities/photo_preview.entity';
 import { QualificationPreview } from './entities/qualification_preview.entity';
@@ -7,6 +8,7 @@ import { Setting } from './entities/setting.entity';
 import { SponcerContainer } from './entities/sponcer_container.entity';
 import { TimelineEvent } from './entities/timeline_event.entity';
 import { PhotoPreviewService } from './photo_preview.service';
+import { QualificationPreviewService } from './qualification_preview.service';
 import { SettingController } from './setting.controller';
 import { SettingService } from './setting.service';
 import { SponcerContainerService } from './sponcer_container.service';
@@ -24,6 +26,13 @@ import { TimelineEventService } from './timeline_event.service';
     ]),
   ],
   controllers: [SettingController],
-  providers: [SettingService, TimelineEventService, SponcerContainerService, PhotoPreviewService],
+  providers: [
+    SettingService,
+    TimelineEventService,
+    SponcerContainerService,
+    PhotoPreviewService,
+    AboutFeContainerService,
+    QualificationPreviewService,
+  ],
 })
 export class SettingModule {}
