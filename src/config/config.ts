f047d8d38184649cbdd7346658f5a2f8d687e833
samithiwat <samithiwat2544@gmail.com>
2022-01-18 08:11:06 +0700

    Add discord webhook ENV

--- src/config/config.ts
+++ src/config/config.ts
@@ -65,4 +65,7 @@ export default () => ({
     publicKey: process.env.OMISE_PUBLIC_KEY,
     secretKey: process.env.OMISE_SECRET_KEY,
   },
+  discord: {
+    webhookUrl: process.env.DISCORD_WEBHOOK_URL,
+  },
 });
