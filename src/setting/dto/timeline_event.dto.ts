fe1c64650833263a3c911d92308976310b4cd79e
sern <sern2001@hotmail.com>
2022-01-20 01:17:31 +0700

    Add isUrl & isISO8601

--- src/setting/dto/timeline_event.dto.ts
+++ src/setting/dto/timeline_event.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsISO8601 } from 'class-validator';
 
 export class TimelineEventDto {
   id: number;
@@ -7,6 +8,7 @@ export class TimelineEventDto {
   text: string;
 
   @ApiProperty()
+  @IsISO8601()
   eventDate: Date;
 
   constructor(partial: Partial<TimelineEventDto>) {
