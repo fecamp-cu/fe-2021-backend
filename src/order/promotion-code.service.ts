dcef00b8cbefa7134b03070a95c53e1fc9b811fd
samithiwat <samithiwat2544@gmail.com>
2022-01-22 15:28:55 +0700

    Add Promotion Code endpoint

--- src/order/promotion-code.service.ts
+++ src/order/promotion-code.service.ts
@@ -23,6 +23,7 @@ export class PromotionCodeService {
   async generate(
     type: PromotionCodeType,
     expiresDate: Date,
+    isReuseable: boolean,
     code: string = crypto
       .SHA256(
         faker.datatype.string(faker.datatype.number(100)) +
@@ -33,12 +34,23 @@ export class PromotionCodeService {
       .substring(0, 10),
     value: number = faker.datatype.number(50),
   ): Promise<PromotionCodeDto> {
+    if (isReuseable && !expiresDate) {
+      throw new BadRequestException({
+        reason: 'MALFORM_INPUT',
+        message: 'You must included expiresDate when isReuseable is true',
+      });
+    }
+
     const promotionCodeDto = new PromotionCodeDto({
       code,
       type,
-      expiresDate,
+      isReuseable,
       value,
     });
+
+    if (expiresDate) {
+      promotionCodeDto.expiresDate = expiresDate;
+    }
 
     const promotionCode = await this.promotionCodeRepository.create(promotionCodeDto);
     await this.promotionCodeRepository.save(promotionCode);
@@ -46,7 +58,7 @@ export class PromotionCodeService {
     return promotionCode;
   }
 
-  async use(code: string): Promise<boolean> {
+  async use(code: string): Promise<PromotionCodeDto> {
     if (!code) {
       throw new BadRequestException({
         reason: 'MALFORM_INPUT',
@@ -77,10 +89,12 @@ export class PromotionCodeService {
       });
     }
 
-    promotionCode.isActived = true;
-    await this.promotionCodeRepository.save(promotionCode);
+    if (!promotionCode.isReuseable) {
+      promotionCode.isActived = true;
+      await this.promotionCodeRepository.save(promotionCode);
+    }
 
-    return true;
+    return promotionCode;
   }
 
   async getPromotionCode(code: string): Promise<PromotionCodeDto> {
