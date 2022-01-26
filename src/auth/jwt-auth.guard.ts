bdb71ba03617dc7b9055bd89d8a97e874a8ff218
samithiwat <samithiwat2544@gmail.com>
2022-01-26 22:22:33 +0700

    Merge redeem auth guard with jwt guard

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,5 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import * as moment from 'moment';
+import { AuthService } from './auth.service';
+import { TokenDto } from './dto/token.dto';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext) {
+    const req = context.switchToHttp().getRequest();
+    const res = context.switchToHttp().getResponse();
+
+    const refreshToken: string = req.cookies['refresh_token'];
+    const getRefreshToken: TokenDto = await this.authService.findRefreshToken(refreshToken);
+
+    if (moment().add(3, 'm').isAfter(moment(getRefreshToken.expiresDate.getTime()))) {
+      const newRefreshToken = await this.authService.createRefreshToken(getRefreshToken.user.id);
+      const newToken = await this.authService.createToken(getRefreshToken.user);
+
+      req.cookies['access_token'] = newToken;
+      res.cookie('access_token', newToken, { httpOnly: true, secure: false });
+      res.cookie('refresh_token', newRefreshToken, { httpOnly: true, secure: false });
+    }
+
+    const valid = await super.canActivate(context);
+
+    if (!valid) {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+}
