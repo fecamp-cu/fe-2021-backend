e3a2a360672cecba87811119ea3989740cc45a47
samithiwat <samithiwat2544@gmail.com>
2022-01-29 19:29:20 +0700

    Rework shop discord message

--- src/common/constants/discord-message.constant.ts
+++ src/common/constants/discord-message.constant.ts
@@ -1,9 +1,29 @@
+import * as moment from 'moment';
 import { OrderDto } from 'src/shop/dto/order.dto';
 
 export const DiscordShopMessage = (orderDto: OrderDto): string => {
   return `
-  คุณ ${orderDto.customer.firstname} ${orderDto.customer.lastname} ได้สั่งซื้อสินค้าเรียบร้อยแล้ว
-  ชนิดของสินค้า คือ ${orderDto.items.map(orderItem => orderItem.item.title).join(' ')}
-  จำนวน ${orderDto.amount} ชิ้น
-  รหัสการสั่งซื้อคือ ${orderDto.id}`;
+  ขายของออกอีกแล้วววววว 🎉🎉🎉🎉
+  คุณ **${orderDto.customer.firstname}** **${
+    orderDto.customer.lastname
+  }** ได้กดสั่งซื้อของจาก ${'**`FE Shop 🤑`**'} เรียบร้อย
+  
+  **📌 ข้อมูลทั่วไป**
+  - **รหัสการสั่งซื้อ**: ${orderDto.id}
+  - **เวลาสั่งซื้อ**: ${moment(orderDto.paidAt).format('dddd, MMMM Do YYYY, h:mm:ss a')}
+  - **จ่ายโดย**: ${orderDto.paymentMethod}
+  
+  **🔍 มีรายการดังนี้** ${formatItem(orderDto)}
+  💰 รวมเป็นเงินทั้งหมด ${'`' + orderDto.amount / 100 + '`'} บาท`;
 };
+
+function formatItem(orderDto: OrderDto) {
+  return `
+  ${orderDto.items
+    .map(
+      orderItem =>
+        `${'`' + orderItem.item.title + '`'} จำนวน ${'`' + orderItem.quantity + '`'} ชิ้น`,
+    )
+    .join('\n')}
+  `;
+}
