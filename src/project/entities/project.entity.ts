462137c8fc90806a6852eda4c68102211c18e983
Sern <sern2001@hotmail.com>
2022-04-03 16:23:08 +0700

    Create project entity

--- src/project/entities/project.entity.ts
+++ src/project/entities/project.entity.ts
@@ -1,1 +1,35 @@
-export class Project {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Project {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ name: 'publish_date' })
+  publishDate: Date;
+
+  @Column({ name: 'end_date' })
+  endDate: Date;
+
+  @Column({ name: 'leader_name' })
+  leaderName: string;
+
+  @CreateDateColumn({ name: 'created_date', select: false })
+  createdDate: Date;
+
+  @UpdateDateColumn({ name: 'updated_date', select: false })
+  updatedDate: Date;
+
+  @DeleteDateColumn({ name: 'deleted_date', select: false })
+  deletedDate: Date;
+}
