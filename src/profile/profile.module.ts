1970f7d85d91832513d93383144c0d07591e4183
samithiwat <samithiwat2544@gmail.com>
2022-04-07 23:19:53 +0700

    Import third party module

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CaslAbilityFactory } from 'src/casl/casl-ability.factory';
+import { ThirdPartyModule } from 'src/third-party/third-party.module';
 import { UserModule } from 'src/user/user.module';
 import { Profile } from './entities/profile.entity';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Profile]), UserModule],
+  imports: [TypeOrmModule.forFeature([Profile]), UserModule, ThirdPartyModule],
   controllers: [ProfileController],
   providers: [ProfileService, CaslAbilityFactory],
   exports: [ProfileService],
