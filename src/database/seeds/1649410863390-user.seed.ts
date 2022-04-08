19c67e15386ad81217b92e8e9737d5c30f0d4e06
samithiwat <samithiwat2544@gmail.com>
2022-04-08 17:11:54 +0700

    Remove token factory from user seed and set seed password

--- src/database/seeds/1649410863390-user.seed.ts
+++ src/database/seeds/1649410863390-user.seed.ts
@@ -1,5 +1,4 @@
 import * as faker from 'faker';
-import { Token } from 'src/auth/entities/token.entity';
 import { User } from 'src/user/entities/user.entity';
 import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
@@ -8,11 +7,10 @@ export default class UserSeed implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
     connection.isConnected;
     for (let i = 0; i < 20; i++) {
-      const tokens = await factory(Token)().createMany(faker.datatype.number({ min: 1, max: 3 }));
       const role = ['user', 'admin'];
       await factory(User)({
+        password: 'adminadmin',
         role: role[faker.datatype.number({ min: 1, max: 2 })],
-        tokens,
       }).create();
     }
   }
